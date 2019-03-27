cd7f9f2a72cf86c80cac63d07f84e57ad418f3a8
JacobTB99 <jb467917@ohio.edu>
2019-03-26 21:02:30 -0400

    Relegated input/output to main function. Changed type of search function from void to int. Main now utilizes returned values from search function. @nasseef

--- search.cc
+++ search.cc
@@ -3,7 +3,7 @@
 * Author: Jacob Beauchamp
 * Date: March, 26, 2019
 * Purpose: Generates an array of random integers, and prints them to the console. Then, prompts
-* the user for a value to search for. If the value appears in the list at least once, its position is printed to console. If it is not
+* the user for a value to search for. If the value appears in the list, its position is printed to console. If it is not
 * present, the user is informed so.
 */
 
@@ -14,12 +14,12 @@
 using namespace std;
 
 //prototypes
-void search(const int array[], int target);
+int search(const int array[], int target);
 /*
 * Description: Sequentially searches an array for the specified target value
 * Parameters: Takes an array of integers, and a target value, which is an integer
 * Preconditions: array must be an array of integers, target must be an integer
-* Postconditions: The function outputs the location(s) in which the target value appears in the array
+* Postconditions: The function returns the integer value of the index location of the target value.
 */
 
 //start
@@ -27,6 +27,7 @@ int main(){
     //declarations
     int randoms[20];
     int target;
+    int location = -1; //assume value nonexistend by default
     //generate values
     srand(time(NULL));
     //run loop to fill elements, and simultaneously print them to console
@@ -39,21 +40,24 @@ int main(){
     //prompt user and call search function
     cout << "Select a target value: ";
     cin >> target;
-    search(randoms, target);
+    location = search(randoms, target);
+    if(location == -1)
+    {
+        cout << "Value not found." << endl;
+        return 0;
+    }
+    cout << "Target value located in position " << location << '.' << endl;
     return 0;
 }
 
-void search(const int array[], int target){
-    bool present = false; //set present to fals by default
+int search(const int array[], int target){
+    //loop for entirety of array
     for(size_t i = 0; i < 20; i++)
     {
-        if(array[i] == target)
+        if(array[i] == target) //return location if value exists
         {
-            cout << "Target value located at position " << i << '.' << endl;
-            present = true; //if target appears at least once, set present to true
+        return i;
         }
     }
-    if(present == false) //only print missing message if it's not present
-    cout << "Target value not present in list." << endl;
-    return;
+    return -1; //if value never found, then return -1
 }
